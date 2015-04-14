bae34e7d331204a106a6d4d5b7fb73d40c160ec2
mvansegbroeck <mvansegbroeck@gmail.com>
2015-04-14 10:35:38 -0700

    first commit

---
+++ apply_vad.m
@@ -0,0 +1,80 @@
+% Example default usage of 'denoise.m'
+% Author: Maarten Van Segbroeck
+
+% set path
+addpath mfiles/
+
+audiodir='../audio/anorexia study/';
+outdir_txt='../pitch/anorexia study/';
+%audiodir='../audio/OCD study/';
+%outdir_txt='../pitch/OCD study/';
+if ~exist(outdir_txt), mkdir(outdir_txt); end
+
+filenames=dir(sprintf('%s/*.wav',audiodir));
+fs=8000;
+
+% ---feature---
+% GT
+NbCh=64;
+% Gabor
+nb_mod_freq=2;
+% LTSV
+R=50; % context
+M=10; % smoothing
+ltsvThr=0.5;
+ltsvSlope=0.2;
+% vprob2 and ltsv2
+K=30; order=4; 
+% -------------
+
+% ---vad model---
+load('models/model.mat')
+
+% ---vad parameters (to tune)---
+p1=0.1;
+p2=40;
+
+% ---visualize---
+visualize=true;
+
+for k = 1 : length(filenames)
+
+ % read in audio
+ [sam,fs_orig]=wavread(fullfile(audiodir,filenames(k).name));
+ sam_8k=downsample(sam(:,1),fs_orig/fs);
+ 
+ % [1] extract cochleagram
+ gt=FE_GT(sam_8k,fs,NbCh);
+
+ % [2] Gabor filtering applied on mel
+ gbf=FE_GBF(sam_8k,fs,nb_mod_freq,false);
+ gbf= [gbf gbf(:,ones(1,10)*size(gbf,2))];
+ gbf = gbf(:,1:size(gt,2));
+
+ % [3] LTSV 
+ ltsv=FE_LTSV(sam_8k,fs,R,M,gt,ltsvThr,ltsvSlope);
+ ltsv2 = convert_to_context_stream(ltsv, K, order);
+ ltsv2= [ltsv2 ltsv2(:,ones(1,10)*size(ltsv,2))];
+ ltsv2 = ltsv2(:,1:size(gt,2));
+
+ % [4] vprob prob
+ vprob=voicingfeature(sam_8k,fs);
+ vprob2 = convert_to_context_stream(vprob, K, order);
+ vprob2 = [vprob2 vprob2(:,ones(1,10)*size(vprob,2))];
+ vprob2 = vprob2(:,1:size(gt,2));
+ 
+ % feature for VAD
+ test_x = [gt;gbf;ltsv2;vprob2];
+ test_x_norm = mvn(test_x);
+ 
+ % VAD decoding
+ [~,~,output] = nntest(dnn, test_x_norm');
+  outprob=double(output(:,1));
+ labelsPerFrame=medfilt1(outprob.^2,p2)>p1;
+ 
+ if visualize
+  imagesc(mvn(gt));axis xy;hold on;
+  plot(10*labelsPerFrame,'m','LineWidth',3); zoom xon; hold off
+  keyboard
+ end
+end
---
+++ mfiles/spmax2mex.c
@@ -0,0 +1,72 @@
+/*
+
+  spmax2.c	max over all non-zero cols of a matrix
+
+   [y,arg]=spmax2(row,col,val1,val2,N)
+is equivalent to [y,arg]=spmax(x)
+where x has N cols and is given by val1+val2 in position(row,col). If val1==-INF => entry disregarded
+-INF in y signify that column was empty.
+
+*/
+
+#include <math.h>
+#include <float.h>
+#include "mex.h"
+
+#define row		prhs[0]
+#define col		prhs[1]
+#define val1		prhs[2]
+#define val2		prhs[3]
+#define NbCol		prhs[4]
+
+#define y		plhs[0]
+#define arg		plhs[1]
+
+void mexFunction(
+	int	nlhs,
+	mxArray	*plhs[],
+	int	nrhs,
+	const mxArray	*prhs[]
+	)
+
+{
+double		*py, *parg;
+double		*prow, *pcol, *pval1, *pval2;
+double		val;
+int		N, NNZ, i;
+
+/* Check for proper number of arguments */
+if (nrhs != 5)
+  mexErrMsgTxt("spmax2 requires five input argument2.");
+
+N=*mxGetPr(NbCol);
+NNZ=mxGetM(row)*mxGetN(row);
+
+if (mxGetM(col)*mxGetN(col) != NNZ) mexErrMsgTxt("row, col, val1 and val2 must be isomorphic.");
+if (mxGetM(val1)*mxGetN(val1) != NNZ) mexErrMsgTxt("row, col, val1 and val2 must be isomorphic.");
+if (mxGetM(val2)*mxGetN(val2) != NNZ) mexErrMsgTxt("row, col, val1 and val2 must be isomorphic.");
+
+y  =mxCreateDoubleMatrix(1,N,mxREAL);
+py =mxGetPr(y);
+for (i=0; i<N; i++) py[i]=-INFINITY;
+
+arg=mxCreateDoubleMatrix(1,N,mxREAL);
+parg=mxGetPr(arg);
+
+prow =mxGetPr(row);
+pcol =mxGetPr(col);
+pval1=mxGetPr(val1);
+pval2=mxGetPr(val2);
+
+for (i=0; i<NNZ ; i++, prow++, pcol++, pval1++, pval2++) {
+	if (*pval1 != -INFINITY) {
+		val = *pval1 + *pval2;
+		if (val > py[(int)(*pcol)-1]) {
+			py[(int)(*pcol)-1]=val;
+			parg[(int)(*pcol)-1]=*prow;
+		}
+	}
+}
+
+}
+
