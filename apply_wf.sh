2ec6352cc780392303e6eaac77e47ba2ec7b8fe5
mvansegbroeck <mvansegbroeck@gmail.com>
2015-04-14 10:53:30 -0700

    Adding Files

--- apply_wf.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-silfile=$1
-samfile=$2
-wffile=$3
-export AURORACALC=aurora-front-end/qio
-./tools/nr -fs 16000 -swapin 0 -swapout 0 -Length 20 -Ssilfile $silfile -i $samfile -o $wffile
